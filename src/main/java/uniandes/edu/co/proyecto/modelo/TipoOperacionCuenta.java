01f11604953be64cc879f515cc012653914829c4
gordosaurio <63026665+gordosaurio@users.noreply.github.com>
2024-04-24 10:01:30 -0500

    Update TipoOperacionCuenta.java

--- src/main/java/uniandes/edu/co/proyecto/modelo/TipoOperacionCuenta.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/TipoOperacionCuenta.java
@@ -1,5 +1,26 @@
 package uniandes.edu.co.proyecto.modelo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "TIPO_OPERACION_CUENTA")
 public class TipoOperacionCuenta {
-    
+    @Id
+    private String tipoOperacionCuenta;
+
+    public TipoOperacionCuenta(){;}
+
+    public TipoOperacionCuenta(String tipoOperacionCuenta)
+    {
+        this.tipoOperacionCuenta = tipoOperacionCuenta;
+    }
+
+    public String getTipoCuenta(){
+        return tipoOperacionCuenta;
+    }  
+
+    public void setTipoCuenta(String tipoOperacionCuenta){
+        this.tipoOperacionCuenta = tipoOperacionCuenta;
+    }
 }
