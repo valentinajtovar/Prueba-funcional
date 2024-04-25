5e78e170a3e8c6102cd5d85ebf482a753685885f
gordosaurio <63026665+gordosaurio@users.noreply.github.com>
2024-04-25 13:02:50 -0500

    Create TipoOperacionPrestamo.java

---
+++ src/main/java/uniandes/edu/co/proyecto/modelo/TipoOperacionPrestamo.java
@@ -0,0 +1,28 @@
+package uniandes.edu.co.proyecto.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TIPO_OPERACION_PRESTAMO")
+public class TipoOperacionPrestamo {
+
+    @Id
+    private String tipoOperacionPrestamo;
+
+    public TipoOperacionPrestamo(){;}
+
+    public TipoOperacionPrestamo(String tipoOperacionPrestamo){
+        this.tipoOperacionPrestamo = tipoOperacionPrestamo;
+    }
+
+    public String getTipoOperacionPrestamo() {
+        return tipoOperacionPrestamo;
+    }
+
+    public void setTipoOperacionPrestamo(String tipoOperacionPrestamo) {
+        this.tipoOperacionPrestamo = tipoOperacionPrestamo;
+    }
+    
+}
