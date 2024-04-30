1a402d5b8277b9c8804bbd5e68ec553c55adf4c9
Valentina Jiménez <77984927+valentinajtovar@users.noreply.github.com>
2024-04-29 23:31:55 -0500

    Add files via upload

--- src/main/java/uniandes/edu/co/proyecto/modelo/TipoDocumento.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/TipoDocumento.java
@@ -1,7 +1,12 @@
 package uniandes.edu.co.proyecto.modelo;
 
+import java.util.Collection;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -9,6 +14,9 @@ import jakarta.persistence.Table;
 public class TipoDocumento {
     @Id
     private String tipoDocumento;
+
+    @OneToMany(fetch = FetchType.LAZY,mappedBy = "tipoDocumento", cascade = CascadeType.ALL)
+    Collection<Usuario> usuarios;
 
     public TipoDocumento(){;}
 
--- src/main/java/uniandes/edu/co/proyecto/modelo/Usuario.java
+++ src/main/java/uniandes/edu/co/proyecto/modelo/Usuario.java
@@ -1,9 +1,8 @@
 package uniandes.edu.co.proyecto.modelo;
 
 
-import java.util.List;
-
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -11,19 +10,24 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 
 @Entity
 @Table(name = "USUARIO")
 public class Usuario {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idUsuario;
 
+
     @ManyToOne
-    @JoinColumn(name ="tipo_documento", referencedColumnName = "tipoDocumento")
+    @JoinColumn(name ="tipoDocumento", referencedColumnName = "tipoDocumento")
     private TipoDocumento tipoDocumento;
 
     private Integer numeroDocumento;
@@ -40,24 +44,21 @@ public class Usuario {
 
     private Integer codigoPostal;
 
+
     @ManyToOne
-    @JoinColumn(name ="tipo_usuario", referencedColumnName = "tipoUsuario")
+    @JoinColumn(name ="tipoUsuario", referencedColumnName = "tipoUsuario")
     private TipoUsuario tipoUsuario;
 
     
-    @OneToOne()
-    @JoinColumn(name ="DATOS_USUARIO", referencedColumnName = "idDatosUsuario")
-    private DatosUsuario datosUsuario; 
-
- 
 
     public Usuario(){;}
 
     
-    public Usuario(Integer idUsuario,TipoDocumento tipoDocumento, Integer numeroDocumento, String nombre,
-            String nacionalidad, String direccionFisica, String direccionDigital, Long telefono,
-            Integer codigoPostal, TipoUsuario tipoUsuario, DatosUsuario datosUsuario) {
-        this.idUsuario=idUsuario;
+
+    public Usuario( TipoDocumento tipoDocumento, Integer numeroDocumento, String nombre,
+            String nacionalidad, String direccionFisica, String direccionDigital, Long telefono, Integer codigoPostal,
+            TipoUsuario tipoUsuario) {
+
         this.tipoDocumento = tipoDocumento;
         this.numeroDocumento = numeroDocumento;
         this.nombre = nombre;
@@ -67,99 +68,139 @@ public class Usuario {
         this.telefono = telefono;
         this.codigoPostal = codigoPostal;
         this.tipoUsuario = tipoUsuario;
-        this.datosUsuario = datosUsuario;
     }
 
-    public Integer getId() {
-        return idUsuario;
-    }
 
-    public void setId(Integer id) {
-        this.idUsuario = id;
-    }
+
+
+
+
+
+
 
     public TipoDocumento getTipoDocumento() {
         return tipoDocumento;
     }
 
+
+
     public void setTipoDocumento(TipoDocumento tipoDocumento) {
         this.tipoDocumento = tipoDocumento;
     }
+
+
 
     public Integer getNumeroDocumento() {
         return numeroDocumento;
     }
 
+
+
     public void setNumeroDocumento(Integer numeroDocumento) {
         this.numeroDocumento = numeroDocumento;
     }
+
+
 
     public String getNombre() {
         return nombre;
     }
 
+
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+
 
     public String getNacionalidad() {
         return nacionalidad;
     }
 
+
+
     public void setNacionalidad(String nacionalidad) {
         this.nacionalidad = nacionalidad;
     }
+
+
 
     public String getDireccionFisica() {
         return direccionFisica;
     }
 
+
+
     public void setDireccionFisica(String direccionFisica) {
         this.direccionFisica = direccionFisica;
     }
+
+
 
     public String getDireccionDigital() {
         return direccionDigital;
     }
 
+
+
     public void setDireccionDigital(String direccionDigital) {
         this.direccionDigital = direccionDigital;
     }
+
+
 
     public Long getTelefono() {
         return telefono;
     }
 
+
+
     public void setTelefono(Long telefono) {
         this.telefono = telefono;
     }
+
+
 
     public Integer getCodigoPostal() {
         return codigoPostal;
     }
 
+
+
     public void setCodigoPostal(Integer codigoPostal) {
         this.codigoPostal = codigoPostal;
     }
+
+
 
     public TipoUsuario getTipoUsuario() {
         return tipoUsuario;
     }
 
+
+
     public void setTipoUsuario(TipoUsuario tipoUsuario) {
         this.tipoUsuario = tipoUsuario;
     }
-    
-    public DatosUsuario getDatosUsuario() {
-        return datosUsuario;
-    }
 
-    public void setDatosUsuario(DatosUsuario datosUsuario) {
-        this.datosUsuario = datosUsuario;
-    }
+
 
     public Integer getIdUsuario() {
         return idUsuario;
     }
+
+
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+
+
+
+
+    
+   
 
 }
